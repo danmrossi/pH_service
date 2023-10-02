5e211148ce4d5e7bb244bcbf1a7d33da6dce1d5f
Daniel <50608357+danmrossi@users.noreply.github.com>
2023-10-02 21:18:20 +0800

    Update pH_fetch_data.cpp
    I made the following changes:
    
    I modified the condition in the if statement to check if saveValue is either NaN (isnan(saveValue)) or less than 2 (saveValue < 2.0).
    If the condition is met (i.e., saveValue is NaN or less than 2), the function returns without saving the value.
    With these changes, the code will now check for NaN or values less than 2 before saving the measurement to the file.
    
    Signed-off-by: Daniel <50608357+danmrossi@users.noreply.github.com>

--- src/pH_fetch_data.cpp
+++ src/pH_fetch_data.cpp
@@ -8,15 +8,17 @@
 #include <linux/i2c-dev.h> // required for constant definitions
 #include "pH_sensor.cpp"
 
-float previousValue = 0.0; // Initialize the previous value
-
 void saveToFile(float saveValue, std::string filename){
-	// std::cout << filename << std::endl;
-	if (isnan(saveValue) || saveValue == 0.0){ return; }
+	// Check if the value is NaN or less than 2
+	if (isnan(saveValue) || saveValue < 2.0) {
+		// Value does not meet the condition, so return without saving
+		return;
+	}
+
 	// open file to save value
 	std::fstream saveFile;
-	saveFile.open(filename,std::ios::out);
-	if(saveFile.is_open()){
+	saveFile.open(filename, std::ios::out);
+	if (saveFile.is_open()) {
 		saveFile << saveValue;  // save value
 		saveFile.close();  // close file
 	}
@@ -25,26 +27,12 @@ void saveToFile(float saveValue, std::string filename){
 //////////
 // main //
 //////////
-int main(int argc, char *argv[]) {
-    // init
-    pHSensor pH_sensor;
-    if (argc > 1) pH_sensor.setAddress(atoi(argv[1]));
-    // read and save
-    std::string filePath = "/var/lib/reef-pi/pH/";
-
-    float pHValue = pH_sensor.readMeasurement(); // Read the pH measurement
-
-    // Check if the pH value is less than 2
-    if (pHValue < 2.0) {
-        // If it's less than 2, use the previous value
-        saveToFile(previousValue, filePath + "pH.dat");
-        std::cout << "pH value less than 2. Using previous value: " << previousValue << std::endl;
-    } else {
-        // If it's greater than or equal to 2, save the value and update the previous value
-        saveToFile(pHValue, filePath + "pH.dat");
-        previousValue = pHValue;
-        std::cout << "pH value saved: " << pHValue << std::endl;
-    }
-
-    return 0;
+int main(int argc, char *argv[])
+{
+	// init
+	pHSensor pH_sensor;
+	if (argc > 1) pH_sensor.setAddress(atoi(argv[1]));
+	// read and save
+	std::string filePath = "/var/lib/reef-pi/pH/";
+	saveToFile(pH_sensor.readMeasurement(),filePath+"pH.dat");
 }
