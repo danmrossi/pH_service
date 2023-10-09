097fcb9957f3d7cadb5ac1e5eefda49250f485c1
Daniel <50608357+danmrossi@users.noreply.github.com>
2023-10-09 13:32:24 +0800

    Update install_pH.sh
    Signed-off-by: Daniel <50608357+danmrossi@users.noreply.github.com>

--- install_pH.sh
+++ install_pH.sh
@@ -19,7 +19,7 @@
     echo "limitations under the License."
     echo ""
     echo ""
-    echo "press any key to INSTALL..."
+    echo "Press the ENTER key to INSTALL..."
     echo ""
     echo ""
     read input
